fda247fb7eed476486f9d2362d2de265055ce4d8
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-02-25 16:29:19 +0200

    add second slider

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,12 +1,20 @@
 const sliderCreate = () => {
-  const sliderContacts = document.querySelector('[data-slider="slider-connect"]')
+  const sliderContacts = document.querySelector('[data-slider="slider-connect"]');
+  const sliderServices = document.querySelector('[data-slider="slider-services"]');
 
   if (window.matchMedia('(min-width: 1440px)').matches) {
-    document.querySelector('.connect__slider').classList.remove('swiper-wrapper');
-    document.querySelector('.connect').classList.remove('swiper');
-    sliderContacts.removeAttribute('data-slider');
-    const slides = document.querySelectorAll('.connect__slide');
+    const swipers = document.querySelectorAll('.swiper');
+    swipers.forEach(swiper => swiper.classList.remove('swiper'));
+
+    const swiperWrappers = document.querySelectorAll('.swiper-wrapper');
+    swiperWrappers.forEach(swiperWrapper => swiperWrapper.classList.remove('swiper-wrapper'));
+    
+    const slides = document.querySelectorAll('.swiper-slide');
     slides.forEach(slide => slide.classList.remove('swiper-slide'));
+
+    sliderContacts.removeAttribute('data-slider');
+    sliderServices.removeAttribute('data-slider');
+
   } else {
     sliderConnect = new Swiper('[data-slider="slider-connect"]', {
       speed: 1500,
@@ -20,6 +28,24 @@ const sliderCreate = () => {
         delay: 2000,
         disableOnInteraction: false,
       }
+    });
+    sliderServices = new Swiper('[data-slider="slider-services"]', {
+      speed: 1500,
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      spaceBetween: 30,
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+        }
+      },
     });
   }
 }
