195cd4c649f6c073dc38e423c347f2f1f850e2ad
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-02-25 14:39:21 +0200

    adjust slider to media

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,13 +1,25 @@
-const sliderConnect = new Swiper('[data-slider="slider-connect"]', {
-  speed: 1500,
-  loop: true,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  spaceBetween: 30,
-  autoplay: {
-    delay: 2000,
-    disableOnInteraction: false,
+const sliderCreate = () => {
+  const sliderContacts = document.querySelector('[data-slider="slider-connect"]')
+
+  if (window.matchMedia('(min-width: 1440px)').matches) {
+    document.querySelector('.connect__slider').classList.remove('swiper-wrapper');
+    sliderContacts.removeAttribute('data-slider');
+  } else {
+    sliderConnect = new Swiper('[data-slider="slider-connect"]', {
+      speed: 1500,
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      spaceBetween: 30,
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      }
+    });
   }
-});
+}
+
+window.addEventListener('resize', sliderCreate);
+window.addEventListener('load', sliderCreate);
