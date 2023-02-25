f7ef40a6b3debb057ed7637d701ca41a3f3b79bf
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-02-25 09:43:38 +0000

    Deploying to gh-pages from @ Slavyasanek/Hydra-VR@ca233c9d2dc3d69f0dc90cf496980f06ef7eb236 🚀

--- index.211d0393.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".header");e.offsetTop;window.addEventListener("scroll",(function(){Number.parseInt(window.pageYOffset)>=190?e.classList.add("header--background"):e.classList.remove("header--background")}))}();
-//# sourceMappingURL=index.211d0393.js.map
