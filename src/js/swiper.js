cebae1913695c3b5bac921963453a74c7c2e4054
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-02-26 15:40:41 +0200

    add tech slider

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,6 +1,7 @@
 const sliderCreate = () => {
   let sliderContacts = document.querySelector('[data-slider="slider-connect"]');
   let sliderServices = document.querySelector('[data-slider="slider-services"]');
+  let sliderTech = document.querySelector('[data-slider="slider-tech"]');
 
   if (window.matchMedia('(min-width: 1440px)').matches) {
     const swipers = document.querySelectorAll('.swiper');
@@ -14,6 +15,7 @@ const sliderCreate = () => {
 
     sliderContacts.removeAttribute('data-slider');
     sliderServices.removeAttribute('data-slider');
+    sliderTech.removeAttribute('data-slider');
 
   } else {
     sliderContacts = new Swiper('[data-slider="slider-connect"]', {
@@ -30,6 +32,24 @@ const sliderCreate = () => {
       }
     });
     sliderServices = new Swiper('[data-slider="slider-services"]', {
+      speed: 1500,
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      spaceBetween: 30,
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+        }
+      },
+    });
+    sliderTech = new Swiper('[data-slider="slider-services"]', {
       speed: 1500,
       loop: true,
       navigation: {
