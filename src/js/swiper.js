d20d89cd21d8458abb4072646d6b6f9601e9ddad
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-02-27 13:09:58 +0200

    remake slider disabling at media

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,56 +1,20 @@
 const sliderCreate = () => {
-  let sliderContacts = document.querySelector('[data-slider="slider-connect"]');
-  let sliderServices = document.querySelector('[data-slider="slider-services"]');
-  let sliderTech = document.querySelector('[data-slider="slider-tech"]');
-  if (window.matchMedia('(min-width: 1440px)').matches) {
-    const swipers = document.querySelectorAll('.swiper');
-    swipers.forEach(swiper => swiper.classList.remove('swiper'));
+  const sliders = document.querySelectorAll('[data-slider]')
+  const swipers = document.querySelectorAll('.swiper');
 
-    const swiperWrappers = document.querySelectorAll('.swiper-wrapper');
-    swiperWrappers.forEach(swiperWrapper => swiperWrapper.classList.remove('swiper-wrapper'));
-    
-    const slides = document.querySelectorAll('.swiper-slide');
-    slides.forEach(slide => slide.classList.remove('swiper-slide'));
-
-    sliderContacts.removeAttribute('data-slider');
-    sliderServices.removeAttribute('data-slider');
-    sliderTech.removeAttribute('data-slider');
-
-  } else if (!window.matchMedia('(min-width: 1440px)').matches && !sliderContacts) {
-    // * sliderContacts
-    sliderContacts = document.querySelector('.connect');
-    sliderContacts.setAttribute('data-slider', 'slider-connect');
-    sliderContacts.classList.add('swiper');
-    sliderContacts.firstElementChild.classList.add('swiper-wrapper');
-    sliderContacts.querySelectorAll('.connect__slide').forEach(slide => slide.classList.add('swiper-slide'))
-    // * sliderServices
-    sliderServices = document.querySelector('.slider__services-swiper');
-    sliderServices.setAttribute('data-slider', 'slider-services');
-    sliderServices.classList.add('swiper');
-    sliderServices.firstElementChild.classList.add('swiper-wrapper');
-    sliderServices.querySelectorAll('.services__slide').forEach(slide => slide.classList.add('swiper-slide'))
-    // * sliderTech
-    sliderTech = document.querySelector('.technologies__slider-swiper');
-    sliderTech.setAttribute('data-slider', 'slider-tech');
-    sliderTech.classList.add('swiper');
-    sliderTech.firstElementChild.classList.add('swiper-wrapper');
-    sliderTech.querySelectorAll('.technologies__slider-slide').forEach(slide => slide.classList.add('swiper-slide'))
-
-  } else {
-    sliderContacts = new Swiper('[data-slider="slider-connect"]', {
-      speed: 1500,
-      loop: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      spaceBetween: 30,
-      autoplay: {
-        delay: 2000,
-        disableOnInteraction: false,
-      }
+  if (window.matchMedia('(min-width: 1440px)').matches && swipers) {
+    swipers.forEach(swiper => {
+      swiper.firstElementChild.classList.add('disabled');
+      swiper.classList.remove('swiper')
     });
-    sliderServices = new Swiper('[data-slider="slider-services"]', {
+  } else {
+    sliders.forEach(slider => slider.classList.add('swiper'));
+    swipers.forEach(swiper => {
+      if (swiper.firstElementChild.classList.contains('disabled')) {
+        swiper.firstElementChild.classList.remove('disabled');
+      }
+    })
+    const sliderContacts = new Swiper('[data-slider="slider-connect"]', {
       speed: 1500,
       loop: true,
       navigation: {
@@ -63,12 +27,36 @@ const sliderCreate = () => {
         disableOnInteraction: false,
       },
       breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        1440: {
+          slidesPerView: 3,
+        }
+      }
+    });
+    const sliderServices = new Swiper('[data-slider="slider-services"]', {
+      speed: 1500,
+      loop: false,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      spaceBetween: 30,
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
         768: {
           slidesPerView: 2,
+        },
+        1440: {
+          slidesPerView: 4,
         }
       },
     });
-    sliderTech = new Swiper('[data-slider="slider-tech"]', {
+    const sliderTech = new Swiper('[data-slider="slider-tech"]', {
       speed: 2000,
       loop: true,
       navigation: {
